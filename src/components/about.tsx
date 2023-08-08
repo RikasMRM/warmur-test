702e4b71247816c4b57de444f1382dbad62af003
Rikas <rikasrkf@gmail.com>
2023-08-09 02:51:31 +0530

    depolyment issue fixed

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -19,11 +19,11 @@ export default function About() {
     >
       <SectionHeading>About us</SectionHeading>
       <p className="mb-3">
-        <span className="font-medium">Warmur</span>Warmur is a new renewable
+        <span className="font-medium">Warmur</span>is a new renewable
         energy calculator, due to be released late in October 2023. Our mission
         is to help home owners navigate the journey to a carbon free home. We
         can even help you find an installer for your project. By signing up from
-        the homepage, you'll be the first know when our calculator is ready for
+        the homepage, you&apos;ll be the first know when our calculator is ready for
         use.
         <br />
         <br />
@@ -34,10 +34,10 @@ export default function About() {
         also model the impact of installing a battery. Others still can give you
         an idea of your heat pump running costs. A few calculators combine all
         of these technologies to give you a more complete view. By answering a
-        few simple questions about your home and habits, we'll show you your
+        few simple questions about your home and habits, we&apos;ll show you your
         projected savings from combining not just solar, battery & heat pump,
         but also off-peak electricty tarrifs, a crucial component of considering
-        your future home energy bills. It's our mission to build the most
+        your future home energy bills. It&apos;s our mission to build the most
         intuitive home energy calculator, helping you decarbonise your home,
         minimising running costs. Warmur is the missing calculator for
         navigating your path to a #NetZero home.
