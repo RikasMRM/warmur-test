312c87b2a3d860362eb93f567821bed1d846aba0
Rikas <rikasrkf@gmail.com>
2023-08-15 00:01:04 +0530

    commented ghost cms

--- src/app/api/ghost-config.ts
+++ src/app/api/ghost-config.ts
@@ -1,34 +1,34 @@
-import GhostContentAPI from "@tryghost/content-api";
+// import GhostContentAPI from "@tryghost/content-api";
 
-const ghostURL = process.env.GHOST_URL;
-const ghostKey = process.env.GHOST_KEY;
+// const ghostURL = process.env.GHOST_URL;
+// const ghostKey = process.env.GHOST_KEY;
 
-if (!ghostURL || !ghostKey) {
-  console.error(
-    "GHOST_URL and GHOST_KEY environment variables must be defined."
-  );
-  process.exit(1);
-}
+// if (!ghostURL || !ghostKey) {
+//   console.error(
+//     "GHOST_URL and GHOST_KEY environment variables must be defined."
+//   );
+//   process.exit(1);
+// }
 
-const api = new GhostContentAPI({
-  url: ghostURL,
-  key: ghostKey,
-  version: "v5.0",
-});
+// const api = new GhostContentAPI({
+//   url: ghostURL,
+//   key: ghostKey,
+//   version: "v5.0",
+// });
 
-export async function getPosts() {
-  return await api.posts
-    .browse({
-      include: ["tags", "authors"],
-      limit: 10,
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
-}
+// export async function getPosts() {
+//   return await api.posts
+//     .browse({
+//       include: ["tags", "authors"],
+//       limit: 10,
+//     })
+//     .catch((err) => {
+//       throw new Error(err);
+//     });
+// }
 
-export async function getPost(slug: string) {
-  return await api.posts.read({ slug }).catch((err) => {
-    throw new Error(err);
-  });
-}
+// export async function getPost(slug: string) {
+//   return await api.posts.read({ slug }).catch((err) => {
+//     throw new Error(err);
+//   });
+// }
