62413704ace9ef2810871e8abeac51a919de7567
Rikas <rikasrkf@gmail.com>
2023-08-18 00:21:27 +0530

    bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,6 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -30,7 +29,6 @@ export default function RootLayout({
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
-        <SessionProvider>
           <ThemeContextProvider>
             <ActiveSectionContextProvider>
               <Header />
@@ -41,7 +39,6 @@ export default function RootLayout({
               <ThemeSwitch />
             </ActiveSectionContextProvider>
           </ThemeContextProvider>
-        </SessionProvider>
       </body>
     </html>
   );
